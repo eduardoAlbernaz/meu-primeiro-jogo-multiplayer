808e1f8da75d360d9718dda021f79e91ee5d4b13
eduardoAlbernaz <edu.albernaz@gmail.com>
2020-04-16 04:29:46 +0000

    Express + Node +  Socket.io

--- playground/gameName/public/game.js
+++ playground/gameName/public/game.js
@@ -4,48 +4,94 @@ export default function createGame() {
                 fruits: {},
                 screen: {
                     width: 10,
-                    height:10
+                    height: 10
                 }
             }
 
-            function addPlayer(command){
+            const observers = []
+
+            function start() {
+                const frequency = 2000
+
+                setInterval(addFruit, frequency)
+             }
+             
+            function subscribe(observerFunction) {
+                observers.push(observerFunction)
+            }
+
+            function notifyAll(command) {
+                for (const observerFunction of observers) {
+                    observerFunction(command)
+                }
+            }
+
+            function setState(newState) {
+                Object.assign(state, newState)
+            }
+            
+            function addPlayer(command) {
                 const playerId = command.playerId
-                const playerX = command.playerX
-                const playerY = command.playerY
+                const playerX = 'playerX' in command ? command.playerX : Math.floor(Math.random() * state.screen.width)
+                const playerY = 'playerY' in command ? command.playerY : Math.floor(Math.random() * state.screen.height)
 
                 state.players[playerId] = {
                     x: playerX,
                     y: playerY
                 }
+
+                notifyAll({
+                    type: 'add-player',
+                    playerId: playerId,
+                    playerX: playerX,
+                    playerY: playerY
+                })
             }
 
             function removePlayer(command) {
                 const playerId = command.playerId
 
                 delete state.players[playerId]
+
+                notifyAll({
+                    type: 'remove-player',
+                    playerId: playerId
+                })
             }
 
-            function addFruit(command){
-                const fruitId = command.fruitId
-                const fruitX = command.fruitX
-                const fruitY = command.fruitY
+          function addFruit(command) {
+        const fruitId = command ? command.fruitId : Math.floor(Math.random() * 10000000)
+        const fruitX = command ? command.fruitX : Math.floor(Math.random() * state.screen.width)
+        const fruitY = command ? command.fruitY : Math.floor(Math.random() * state.screen.height)
 
-                state.fruits[fruitId] = {
-                    x: fruitX,
-                    y: fruitY
-                }
-            }
+        state.fruits[fruitId] = {
+            x: fruitX,
+            y: fruitY
+        }
+
+        notifyAll({
+            type: 'add-fruit',
+            fruitId: fruitId,
+            fruitX: fruitX,
+            fruitY: fruitY
+        })
+    }
 
             function removeFruit(command) {
                 const fruitId = command.fruitId
 
                 delete state.fruits[fruitId]
+
+                notifyAll({
+            type: 'remove-fruit',
+            fruitId: fruitId,
+        })
             }
 
             function movePlayer(command) {
-    
+                notifyAll(command)
+
                 const accpetedMoves = {
-                    
                     ArrowUp(player) {
                         if (player.y - 1 >= 0) {
                             player.y -= 1
@@ -69,16 +115,16 @@ export default function createGame() {
                 }
 
                 const keyPressed = command.keyPressed
-                const player = state.players[command.playerId]
+                const playerId = command.playerId
+                const player = state.players[playerId]
                 const moveFunction = accpetedMoves[keyPressed]
 
                 if (player && moveFunction) {
                     moveFunction(player)
-                    checkForFruitCollision()
+                    checkForFruitCollision(playerId)
                 }
             }
-            function checkForFruitCollision() {
-                for (const playerId in state.players) {
+            function checkForFruitCollision(playerId) {
                     const player = state.players[playerId]
                     
                         for (const fruitId in state.fruits) {
@@ -86,12 +132,11 @@ export default function createGame() {
                             console.log(`Checking ${playerId} and ${fruitId}`)
 
                             if (player.x === fruit.x && player.y === fruit.y) {
-                                console.log(`COLLISION!!!! between ${playerId} and ${fruitId}`)
+                                console.log(`COLLISION between ${playerId} and ${fruitId}`)
                                 removeFruit({ fruitId: fruitId })
                             }
                         }
                 }
-            }
 
             return {
                 addPlayer,
@@ -99,6 +144,9 @@ export default function createGame() {
                 movePlayer,
                 addFruit,
                 removeFruit,
-                state
+                state,
+                setState,
+                subscribe,
+                start
             }
         }
--- playground/gameName/public/render-screen.js
+++ playground/gameName/public/render-screen.js
@@ -1,6 +1,6 @@
-export default function renderScreen(screen, game, requestAnimationFrame) {
+export default function renderScreen(screen, game, requestAnimationFrame, currentPlayerId) {
     const context = screen.getContext('2d')
-    context.fiilStyle = 'white'
+    context.fillStyle = 'white'
     context.clearRect(0, 0, 10, 10)
 
     for (const playerId in game.state.players) {
@@ -11,11 +11,18 @@ export default function renderScreen(screen, game, requestAnimationFrame) {
 
     for (const fruitId in game.state.fruits) {
         const fruit = game.state.fruits[fruitId]
-        context.fillStyle = 'green'
+        context.fillStyle = 'Coral'
         context.fillRect(fruit.x, fruit.y, 1, 1)
     }
 
+    const currentPlayer = game.state.players[currentPlayerId]
+
+    if(currentPlayer) {
+        context.fillStyle = 'SpringGreen'
+        context.fillRect(currentPlayer.x, currentPlayer.y, 1, 1)
+    }
+
     requestAnimationFrame(() => {
-        renderScreen(screen, game, requestAnimationFrame)
+        renderScreen(screen, game, requestAnimationFrame, currentPlayerId)
     })
 }
